8d07534ec12897fc19dcf7ab299e1771fe763fa9
Vitor Mussa <vtrmussa@gmail.com>
2021-02-03 21:59:08 -0300

    Pythoniza e transforma script do whatsapp em modulo

--- whatsapp/conversas_para_csv.py
+++ whatsapp/conversas_para_csv.py
@@ -9,19 +9,27 @@ import pandas as pd
 import re
 import os
 
-# %% lê o arquivo da conversa
-path_to_file = input('Insira o local do arquivo: ')
-assert os.path.exists(path_to_file), \
-    'Não é possível encontrar o arquivo em: .' + path_to_file
-with open(path_to_file, 'r', encoding='utf-8') as f:
-    txt = ''.join(f.readlines())
+def parse_chat(txt):
+    """Transforma txt bruto em objeto processado pelo modulo re.
+    Separa dados por data, hora, autor e conteudo da mensagem."""
+    pattern = r'(\d\d\/\d\d\/\d\d\d\d) (\d\d:\d\d) - (.*?): (.*)'
+    result = re.findall(pattern, txt)
+    return result
 
-# %% processa o texto e o separa segundo data, hora, autor e conteúdo
-pattern = r'(\d\d\/\d\d\/\d\d\d\d) (\d\d:\d\d) - (.*?): (.*)'
-result = re.findall(pattern, txt)
 
-# %% cria DataFrame
-df = pd.DataFrame(result, columns=['data', 'hora', 'autor', 'conteudo'])
+if __name__ == '__main__':
+    # %% lê o arquivo da conversa
+    path_to_file = input('Insira o local do arquivo: ')
+    assert os.path.exists(path_to_file), \
+        'Não é possível encontrar o arquivo em: .' + path_to_file
+    with open(path_to_file, 'r', encoding='utf-8') as f:
+        txt = ''.join(f.readlines())
 
-# %% exporta DataFrame com dados da conversa para CSV
-df.to_csv(f'{os.path.dirname(__file__)}{os.sep}output{os.sep}conversa.csv', index=False)
+    # %% processa texto da conversa
+    data = parse_chat(txt)
+
+    # %% cria DataFrame
+    df = pd.DataFrame(data, columns=['data', 'hora', 'autor', 'conteudo'])
+
+    # %% exporta DataFrame com dados da conversa para CSV
+    df.to_csv(f'{os.path.dirname(__file__)}{os.sep}output{os.sep}conversa.csv', index=False)
